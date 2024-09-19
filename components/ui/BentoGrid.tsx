e231cb8aee46748c8026bb5bb88b70ccf9e17332
Hendryck Lima <86539955+HendryckDev@users.noreply.github.com>
2024-09-19 12:56:54 -0300

    Main Grid complete

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/utils/cn";
+import { Meteors } from "./meteors";
 
 export const BentoGrid = ({
     className,
@@ -43,7 +44,7 @@ export const BentoGridItem = ({
     return (
         <div
             className={cn(
-                "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+                "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border border-[#4C298D]",
                 className
             )}
             style={{
@@ -61,15 +62,41 @@ export const BentoGridItem = ({
                     {spareImg && (
                         <img src={spareImg} alt={spareImg} className={'object-cover, object-center w-full h-full'} />
                     )}
-                </div>
-            </div>
 
-            <div className="group-hover/bento:translate-x-2 transition duration-200">
-                <div className="font-sans font-bold text-neutral-100 dark:text-neutral-200 mb-2 mt-2">
-                    {title}
                 </div>
-                <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
-                    {description}
+                {id === 3 && (
+                    <div className="">
+                        <div className=" w-full relative">
+                            <div className="absolute h-full w-full transform scale-[0.80]" />
+                            <div className="absolute px-48 py-40 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
+                                <Meteors className="absolute" number={10} />
+                            </div>
+                        </div>
+                    </div>
+                )}
+
+                <div className={cn(
+                    titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:10 justify-center h-full'
+                )}>
+                    <div className="font-sans font-extralight text-[#C1C2D3] text-sm md:text-xs lg:text-base z-10">
+                        {description}
+                    </div>
+                    <div className="font-sans font-bold text-white text-lg lg:text-3xl max-w-96 z-10">
+                        {title}
+                    </div>
+
+                    {id === 2 && (
+                        <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+                            <div className="flex flex-col gap-3 lg:gap-8">
+                                {['React.js', 'Next.js', 'Typescript'].map((item) => (<span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center text-white bg-[#130036]"> {item} </span>))}
+                                <span className="py-4 px-3 rounded-lg text-center bg-[#130036]" />
+                            </div>
+                            <div className="flex flex-col gap-3 lg:gap-8">
+                                <span className="py-4 px-3 rounded-lg text-center bg-[#130036]" />
+                                {['Javascript', 'MySQL', 'Tailwind'].map((item) => (<span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center text-white bg-[#130036]"> {item} </span>))}
+                            </div>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -162,6 +162,19 @@ const config = {
           "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
       },
     },
+    animation: {
+      "meteor-effect": "meteor 5s linear infinite",
+    },
+    keyframes: {
+      meteor: {
+        "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
+        "70%": { opacity: "1" },
+        "100%": {
+          transform: "rotate(215deg) translateX(-500px)",
+          opacity: "0",
+        },
+      },
+    },
   },
   plugins: [
     require("tailwindcss-animate"),
